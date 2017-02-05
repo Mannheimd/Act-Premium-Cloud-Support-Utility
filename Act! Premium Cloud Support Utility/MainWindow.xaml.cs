1b6ad2b976d21e19be5ddc485e5a859e2e6459dc
Mannheimd <cmanders159@gmail.com>
2017-02-05 21:00:56 +0000

    Test build, posts LookupCustomer
    Posts a request to build LookupCustomer job with given parameters

--- Act! Premium Cloud Support Utility/MainWindow.xaml.cs
+++ Act! Premium Cloud Support Utility/MainWindow.xaml.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Act__Premium_Cloud_Support_Utility
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const string URL = @"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters";
+        private string urlParameters = "?LookupCustomerBy=SiteName&LookupValue=keithV19Test1";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Creating base URL
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            // Adding accept header for XML format
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            // Adding authentication details
+            byte[] creds = UTF8Encoding.UTF8.GetBytes("Redacted");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(creds));
+
+            // List data response
+            var response = client.PostAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters?LookupCustomerBy=SiteName&LookupValue=keithV19Test1", new StringContent(""));
         }
     }
 }
