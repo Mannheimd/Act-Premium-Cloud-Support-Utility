eeaf03dcb191f33f77adf302064ff668a9c9981d
Mannheimd <cmanders159@gmail.com>
2017-02-16 19:11:26 +0000

    LookupCustomer improvements
    Improved the way I'm picking up the job I've created - turns out this is
    returned from the original Post in the Location header, so I'm using
    this to check on job progress. Currently loops infinitely with a message
    box, next step is to use the queue lookup output to end the loop at the
    correct time.

--- Act! Premium Cloud Support Utility/MainWindow.xaml.cs
+++ Act! Premium Cloud Support Utility/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Xml;
@@ -37,36 +39,27 @@ namespace Act__Premium_Cloud_Support_Utility
                 // Adding authentication details
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedCreds);
 
-                // Get the build number of the last run job
-                HttpResponseMessage lastBuild = await client.GetAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/lastBuild/buildNumber");
-                int buildNumber = (Convert.ToInt32(await lastBuild.Content.ReadAsStringAsync())) + 1; // Incrementing the build number by 1
-
                 // Post a request to build LookupCustomer and wait for a response
                 HttpResponseMessage response = await client.PostAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters?LookupCustomerBy=" + comboBox_LookupBy.SelectedValue.ToString() + "&LookupValue=" + textBox_LookupValue.Text, new StringContent(""));
 
                 if (response.IsSuccessStatusCode)
                 {
-                    // Get the output from buildNumber +1, then check against the parameters we used to ensure it's the correct one (in case of overlap)
-                    HttpResponseMessage newBuild = await client.GetAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/" + buildNumber.ToString() + @"/api/xml");
-                    XmlDocument buildOutputXml = new XmlDocument();
-                    buildOutputXml.LoadXml(await newBuild.Content.ReadAsStringAsync());
+                    bool buildComplete = false;
 
-                    // Getting the parameters into an XmlNodeList and comparing them with what we expect to see
-                    XmlNodeList parameterNodes = buildOutputXml.SelectNodes(@"freeStyleBuild/action/parameter");
-                    int criteriaMet = 0;
-                    foreach (XmlNode parameter in parameterNodes)
+                    while (!buildComplete)
                     {
-                        XmlNode name = parameter.SelectSingleNode("name");
-                        XmlNode value = parameter.SelectSingleNode("value");
-                        // Increment criteriaMet for each property that matches with what was expected - should result in CriteriaMet being 2
-                        if (name.InnerXml == "LookupCustomerBy" & value.InnerXml == comboBox_LookupBy.SelectedValue.ToString()) criteriaMet++;
-                        if (name.InnerXml == "LookupValue" & value.InnerXml == textBox_LookupValue.Text) criteriaMet++;
-                    }
+                        await Delay(1000);
 
-                    if (criteriaMet == 2)
-                    {
-                        MessageBox.Show("Success!!!");
+                        HttpClient newBuild = new HttpClient();
+                        newBuild.BaseAddress = response.Headers.Location;
+                        string newBuildURL = response.Headers.Location.AbsoluteUri;
+                        HttpResponseMessage newBuildOutput = await client.GetAsync(newBuildURL + @"\api\xml");
+                        MessageBox.Show(await newBuildOutput.Content.ReadAsStringAsync());
                     }
+                }
+                else
+                {
+                    MessageBox.Show(response.ReasonPhrase);
                 }
             }
         }
@@ -87,6 +80,11 @@ namespace Act__Premium_Cloud_Support_Utility
             comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("Subscription Number", "ZuoraSubscription"));
             comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("IIT ID", "IITID"));
             comboBox_LookupBy.SelectedIndex = 0;
+        }
+
+        async Task Delay(int time)
+        {
+            await Task.Delay(time);
         }
     }
 }
