1ff2332b4a018b8a4c5a88bb2ab2ca224aad523b
Mannheimd <cmanders159@gmail.com>
2017-02-16 17:40:25 +0000

    LookupCustomer job expanded
    - Implemented user fields to run job with particular parameters
    
    - Uses last build number to work out what next build number will be
    
    - Partially implemented a check to ensure that the correct build number
    is being looked up after running the job, this doesn't seem to be
    working currently though

--- Act! Premium Cloud Support Utility/MainWindow.xaml.cs
+++ Act! Premium Cloud Support Utility/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
 
 namespace Act__Premium_Cloud_Support_Utility
 {
@@ -13,27 +16,77 @@ namespace Act__Premium_Cloud_Support_Utility
         {
             InitializeComponent();
 
+            PopulateDropDowns();
+
             JenkinsLogin loginWindow = new JenkinsLogin();
             loginWindow.Show();
         }
 
         private async void RunLookupCustomer()
         {
-            string URL = @"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters";
+            if (textBox_LookupValue.Text != "")
+            {
+                // Creating base URL
+                string URL = @"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters";
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(URL);
 
-            // Creating base URL
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
+                // Adding accept header for XML format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
 
-            // Adding accept header for XML format
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/xml"));
+                // Adding authentication details
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedCreds);
 
-            // Adding authentication details
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedCreds);
+                // Get the build number of the last run job
+                HttpResponseMessage lastBuild = await client.GetAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/lastBuild/buildNumber");
+                int buildNumber = (Convert.ToInt32(await lastBuild.Content.ReadAsStringAsync())) + 1; // Incrementing the build number by 1
 
-            // List data response
-            var response = await client.PostAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters?LookupCustomerBy=SiteName&LookupValue=keithV19Test1", new StringContent(""));
+                // Post a request to build LookupCustomer and wait for a response
+                HttpResponseMessage response = await client.PostAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/buildWithParameters?LookupCustomerBy=" + comboBox_LookupBy.SelectedValue.ToString() + "&LookupValue=" + textBox_LookupValue.Text, new StringContent(""));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Get the output from buildNumber +1, then check against the parameters we used to ensure it's the correct one (in case of overlap)
+                    HttpResponseMessage newBuild = await client.GetAsync(@"https://cloudops-jenkins-ust1.hostedtest.act.com:8443/job/CloudOps1-LookupCustomer/" + buildNumber.ToString() + @"/api/xml");
+                    XmlDocument buildOutputXml = new XmlDocument();
+                    buildOutputXml.LoadXml(await newBuild.Content.ReadAsStringAsync());
+
+                    // Getting the parameters into an XmlNodeList and comparing them with what we expect to see
+                    XmlNodeList parameterNodes = buildOutputXml.SelectNodes(@"freeStyleBuild/action/parameter");
+                    int criteriaMet = 0;
+                    foreach (XmlNode parameter in parameterNodes)
+                    {
+                        XmlNode name = parameter.SelectSingleNode("name");
+                        XmlNode value = parameter.SelectSingleNode("value");
+                        // Increment criteriaMet for each property that matches with what was expected - should result in CriteriaMet being 2
+                        if (name.InnerXml == "LookupCustomerBy" & value.InnerXml == comboBox_LookupBy.SelectedValue.ToString()) criteriaMet++;
+                        if (name.InnerXml == "LookupValue" & value.InnerXml == textBox_LookupValue.Text) criteriaMet++;
+                    }
+
+                    if (criteriaMet == 2)
+                    {
+                        MessageBox.Show("Success!!!");
+                    }
+                }
+            }
+        }
+
+        private void button_RunLookupCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            RunLookupCustomer();
+        }
+
+        private void PopulateDropDowns()
+        {
+            // Populating LookupCustomer drop-downs with Key/Value pairs, then setting the selected index to 0
+            comboBox_LookupBy.DisplayMemberPath = "Key";
+            comboBox_LookupBy.SelectedValuePath = "Value";
+            comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("Site Name", "SiteName"));
+            comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("Email Address", "EmailAddress"));
+            comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("Account Number", "ZuoraAccount"));
+            comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("Subscription Number", "ZuoraSubscription"));
+            comboBox_LookupBy.Items.Add(new KeyValuePair<string, string>("IIT ID", "IITID"));
+            comboBox_LookupBy.SelectedIndex = 0;
         }
     }
 }
