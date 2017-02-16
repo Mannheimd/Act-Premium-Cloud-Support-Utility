830abd68bdcb6d7b745ad7e03addc9812a2dc33e
Mannheimd <cmanders159@gmail.com>
2017-02-16 22:21:32 +0000

    Lookup data parsing in place
    Lookup data is now being parsed safely - won't trip up if indivisual
    bits of data aren't as expected, though does mean a slightly less
    efficient search. Will work even if multiple databases are listed.
    
    To do: UI implementation

--- Act! Premium Cloud Support Utility/MainWindow.xaml.cs
+++ Act! Premium Cloud Support Utility/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace Act__Premium_Cloud_Support_Utility
     public partial class MainWindow : Window
     {
         public static string encodedCreds = null; // Encoded version of the user's username and password
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -117,7 +117,23 @@ namespace Act__Premium_Cloud_Support_Utility
                     {
                         HttpResponseMessage finalBuildOutput = await client.GetAsync(newBuildUrl + @"logText/progressiveText?start=0");
 
-                        MessageBox.Show(await finalBuildOutput.Content.ReadAsStringAsync());
+                        string finalBuildText = await finalBuildOutput.Content.ReadAsStringAsync();
+
+                        string iitid = SearchString(finalBuildText, "IITID: ");
+                        string accountName = SearchString(finalBuildText, "Account Name: ");
+                        string email = SearchString(finalBuildText, "Email: ");
+                        string createDate = SearchString(finalBuildText, "Create Date: ");
+                        string trialOrPaid = SearchString(finalBuildText, "Trial or Paid: ");
+                        string serialNumber = SearchString(finalBuildText, "Serial Number: ");
+                        string seatCount = SearchString(finalBuildText, "Seat Count: ");
+                        string suspendStatus = SearchString(finalBuildText, "Suspend status: ");
+                        string archiveStatus = SearchString(finalBuildText, "Archive status: ");
+                        string siteName = SearchString(finalBuildText, "Site Name: ");
+                        string iisServer = SearchString(finalBuildText, "IIS Server: ");
+                        string loginUrl = SearchString(finalBuildText, "URL: ");
+                        string uploadUrl = SearchString(finalBuildText, "Upload: ");
+
+                        List<Database> databaseList = SearchForDatabases(finalBuildText);
                     }
                 }
                 else
@@ -145,9 +161,57 @@ namespace Act__Premium_Cloud_Support_Utility
             comboBox_LookupBy.SelectedIndex = 0;
         }
 
+        private string SearchString(string mainText, string searchFor)
+        {
+            try
+            {
+                string split1 = mainText.Split(new string[] { searchFor }, StringSplitOptions.None)[1];
+                return split1.Split(null)[0];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private List<Database> SearchForDatabases(string mainText)
+        {
+            bool stillGoing = true;
+            string workingText = mainText;
+            List<Database> list = new List<Database>();
+            while(stillGoing)
+            {
+                try
+                {
+                    Database database = new Database();
+
+                    string split1 = workingText.Split(new string[] { "Database: " }, StringSplitOptions.None)[1];
+                    database.name = split1.Split(null)[0];
+                    string split2 = workingText.Split(new string[] { "| Server: " }, StringSplitOptions.None)[1];
+                    database.server = split2.Split(null)[0];
+
+                    list.Add(database);
+
+                    workingText = split2.Split(null)[1];
+                }
+                catch
+                {
+                    stillGoing = false;
+                }
+            }
+
+            return list;
+        }
+
         async Task Delay(int time)
         {
             await Task.Delay(time);
         }
+    }
+
+    public class Database
+    {
+        public string name { get; set; }
+        public string server { get; set; }
     }
 }
